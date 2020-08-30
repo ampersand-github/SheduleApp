5639bb714adf07927ffc4fa12fb495fde14338bf
ampersand <ampersand.dev@gmail.com>
2020-08-31 06:25:17 +0900

    first commit

---
+++ SheduleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SheduleApp
+//
+//  Created by 開発 on 2020/08/31.
+//  Copyright © 2020 開発. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
